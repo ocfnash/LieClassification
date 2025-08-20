3ee3dffe5eeb6bffbe361f43fccc8f929b3f557b
Oliver Nash <github@olivernash.org>
2025-08-20 23:00:34 +0100

    Bump Mathlib

--- LieClassification/LinearAlgebra/RootSystem/CartanMatrix.lean
+++ LieClassification/LinearAlgebra/RootSystem/CartanMatrix.lean
@@ -3,15 +3,20 @@ import Mathlib.LinearAlgebra.RootSystem.CartanMatrix
 
 namespace RootPairing.Base
 
-variable {ι R M N : Type*} [Finite ι] [CommRing R] [IsDomain R] [CharZero R]
+variable {ι R M N : Type*} [Finite ι] [DecidableEq ι] [CommRing R] [IsDomain R] [CharZero R]
   [AddCommGroup M] [Module R M] [AddCommGroup N] [Module R N] {P : RootPairing ι R M N} (b : P.Base)
   [P.IsCrystallographic]
 
 /-- A (finite-type) Cartan matrix cannot have eigenvalue `4`. -/
-lemma cartanMatrix_sub_four_det_ne_zero [Fintype b.support] [DecidableEq b.support] :
-    (b.cartanMatrix - 4 • 1).det ≠ 0 :=
+lemma cartanMatrix_sub_four_det_ne_zero :
+    (4 - b.cartanMatrix).det ≠ 0 :=
   -- Should be able to prove this via Perron-Frobenius as outlined here:
   -- https://leanprover.zulipchat.com/#narrow/channel/116395-maths/topic/Eigenvalues.20of.20Cartan.20matrices/near/516844801
   sorry
+
+-- Once `RootPairing.Base.cartanMatrix_sub_four_det_ne_zero` is proved we can drop this `Fact`
+-- entirely by just using `RootPairing.Base.cartanMatrix_sub_four_det_ne_zero` in the proof of
+-- `RootPairing.GeckConstruction.instIsIrreducible` instead.
+instance : Fact ((4 - b.cartanMatrix).det ≠ 0) := ⟨b.cartanMatrix_sub_four_det_ne_zero⟩
 
 end RootPairing.Base
--- LieClassification/LinearAlgebra/RootSystem/LieAlgebra.lean
+++ LieClassification/LinearAlgebra/RootSystem/LieAlgebra.lean
@@ -1,39 +1,23 @@
 import Mathlib.Algebra.Lie.CartanExists
 import Mathlib.Algebra.Lie.Matrix
 import Mathlib.Algebra.Lie.Semisimple.Lemmas
-import Mathlib.LinearAlgebra.RootSystem.GeckConstruction.Basic
+import Mathlib.LinearAlgebra.RootSystem.GeckConstruction.Semisimple
 import Mathlib.LinearAlgebra.RootSystem.Hom
 import LieClassification.Algebra.Lie.Weights.RootSystem
 import LieClassification.LinearAlgebra.RootSystem.Base
 import LieClassification.LinearAlgebra.RootSystem.CartanMatrix
-import LieClassification.LinearAlgebra.RootSystem.Chain
 
 open LieModule Set
 
 namespace RootPairing
 
 /- In fact `[CommRing R] [IsDomain R]` would suffice for much of the below -/
-variable {ι R M N : Type*} [Finite ι] [Field R] [CharZero R] [IsAlgClosed R]
+variable {ι R M N : Type*} [Fintype ι] [DecidableEq ι] [Field R] [CharZero R] [IsAlgClosed R]
   [AddCommGroup M] [Module R M] [AddCommGroup N] [Module R N]
-  {P : RootSystem ι R M N} [P.IsReduced] [P.IsCrystallographic] [P.IsIrreducible]
+  {P : RootSystem ι R M N} [P.IsReduced] [P.IsCrystallographic] [P.IsIrreducible] [P.IsReduced]
   {b : P.Base}
 
 namespace GeckConstruction
-
--- Easy using `RootPairing.Base.cartanMatrix_nondegenerate`
-lemma linearIndependent_h :
-    LinearIndependent (ι := b.support) R h := by
-  sorry
-
-variable [P.IsReduced] [Fintype ι] [DecidableEq ι]
-
-/-- RootSystemLemma 4.2 from [Geck](Geck2017).
-
-Will need `RootPairing.Base.cartanMatrix_sub_four_det_ne_zero`. -/
-instance : LieModule.IsIrreducible R (lieAlgebra b) (b.support ⊕ ι → R) := sorry
-
-variable (b) in
-abbrev cartanSubalgebra' := (cartanSubalgebra b).comap (lieAlgebra b).incl
 
 instance : (cartanSubalgebra' b).IsCartanSubalgebra := sorry
 
@@ -50,15 +34,7 @@ def lieAlgebraEquiv_of_equiv {ι₂ M₂ N₂ : Type*}
 
 variable (b)
 
-lemma trace_toEnd_eq_zero (x : lieAlgebra b) :
-    LinearMap.trace R _ (toEnd R _ (b.support ⊕ ι → R) x) = 0 :=
-  sorry
-
-/-- Lemma 4.3 from [Geck](Geck2017). -/
-instance : LieAlgebra.HasTrivialRadical R (lieAlgebra b) :=
-  LieAlgebra.hasTrivialRadical_of_isIrreducible_of_isFaithful R _ _ (trace_toEnd_eq_zero b)
-
-/-- `LieAlgebra.IsKilling.rootSystem` is left inverse to `RootSystem.lieAlgebra`.
+/-- `LieAlgebra.IsKilling.rootSystem` is left inverse to `RootPairing.GeckConstruction.lieAlgebra`.
 
 Lemma 4.6 from [Geck](Geck2017). -/
 def rootSystem_cartanSubalgebra_equiv_self :
@@ -66,7 +42,8 @@ def rootSystem_cartanSubalgebra_equiv_self :
   sorry
 
 open scoped Classical in
-/-- `LieAlgebra.IsKilling.rootSystem` is right inverse to `RootSystem.lieAlgebra`. -/
+/-- `LieAlgebra.IsKilling.rootSystem` is right inverse to `RootPairing.GeckConstruction.lieAlgebra`.
+-/
 def _root_.LieAlgebra.equiv_rootsystem_lieAlgebra {R L : Type*} [Field R] [CharZero R]
     [LieRing L] [LieAlgebra R L] [FiniteDimensional R L] [LieAlgebra.IsSimple R L]
     (H : LieSubalgebra R L) [H.IsCartanSubalgebra] [IsTriangularizable R H L]
