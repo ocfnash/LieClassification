fac942b5c093e19103f3d0f5e70b6d87cdd3638d
Oliver Nash <github@olivernash.org>
2026-01-19 11:49:45 +0000

    Bump Mathlib
    Also some minor scope reduction

--- LieClassification/Algebra/Lie/Weights/RootSystem.lean
+++ LieClassification/Algebra/Lie/Weights/RootSystem.lean
@@ -10,6 +10,7 @@ variable {K L : Type*}
   [Field K] [CharZero K] [LieRing L] [LieAlgebra K L] [FiniteDimensional K L]
   (H : LieSubalgebra K L) [H.IsCartanSubalgebra] [IsTriangularizable K H L]
 
+-- Not strictly necessary for easiest version of classification (though highly desirable).
 lemma isSimple_of_isIrreducible_rootSystem [IsSemisimple K L] (h : (rootSystem H).IsIrreducible) :
     IsSimple K L :=
   sorry
--- LieClassification/LinearAlgebra/RootSystem/LieAlgebra.lean
+++ LieClassification/LinearAlgebra/RootSystem/LieAlgebra.lean
@@ -1,44 +1,27 @@
 import Mathlib.Algebra.Lie.CartanExists
-import Mathlib.Algebra.Lie.Matrix
-import Mathlib.Algebra.Lie.Semisimple.Lemmas
+import Mathlib.LinearAlgebra.RootSystem.BaseExists
 import Mathlib.LinearAlgebra.RootSystem.GeckConstruction.Semisimple
-import Mathlib.LinearAlgebra.RootSystem.Hom
-import LieClassification.Algebra.Lie.Weights.RootSystem
-import LieClassification.LinearAlgebra.RootSystem.Base
+import Mathlib.Algebra.Lie.Weights.RootSystem
+import LieClassification.Algebra.Lie.CartanCriterion
 import LieClassification.LinearAlgebra.RootSystem.CartanMatrix
 
-open LieModule Set
+open LieModule Module
 
-namespace RootPairing
+namespace RootPairing.GeckConstruction
 
-/- In fact `[CommRing R] [IsDomain R]` would suffice for much of the below -/
 variable {ι R M N : Type*} [Fintype ι] [DecidableEq ι] [Field R] [CharZero R] [IsAlgClosed R]
   [AddCommGroup M] [Module R M] [AddCommGroup N] [Module R N]
-  {P : RootSystem ι R M N} [P.IsReduced] [P.IsCrystallographic] [P.IsIrreducible] [P.IsReduced]
-  {b : P.Base}
+  {P : RootPairing ι R M N} [P.IsRootSystem] [P.IsReduced] [P.IsCrystallographic] [P.IsIrreducible]
+  (b : P.Base)
 
-namespace GeckConstruction
-
+/-- See Lemma 4.6 from [Geck](Geck2017). -/
 instance : (cartanSubalgebra' b).IsCartanSubalgebra := sorry
-
-/-- Equivalent root systems, give equivalent Lie algebras. -/
-def lieAlgebraEquiv_of_equiv {ι₂ M₂ N₂ : Type*}
-    [AddCommGroup M₂] [Module R M₂] [AddCommGroup N₂] [Module R N₂]
-    {P₂ : RootSystem ι₂ R M₂ N₂} [P₂.IsReduced] [P₂.IsCrystallographic] [P₂.IsIrreducible]
-    {b₂ : P₂.Base} [Fintype ι₂] [DecidableEq ι₂]
-    (e : P.Equiv P₂.toRootPairing) :
-    lieAlgebra b ≃ₗ⁅R⁆ lieAlgebra b₂ := by
-  -- Need to know Weyl group is transitive on bases but otherwise easy:
-  -- construction just uses the values in the pairing.
-  sorry
-
-variable (b)
 
 /-- `LieAlgebra.IsKilling.rootSystem` is left inverse to `RootPairing.GeckConstruction.lieAlgebra`.
 
-Lemma 4.6 from [Geck](Geck2017). -/
+See Lemma 4.6 from [Geck](Geck2017). -/
 def rootSystem_cartanSubalgebra_equiv_self :
-    (LieAlgebra.IsKilling.rootSystem (cartanSubalgebra' b)).Equiv P.toRootPairing :=
+    (LieAlgebra.IsKilling.rootSystem (cartanSubalgebra' b)).Equiv P :=
   sorry
 
 open scoped Classical in
@@ -51,26 +34,20 @@ def _root_.LieAlgebra.equiv_rootsystem_lieAlgebra {R L : Type*} [Field R] [CharZ
     L ≃ₗ⁅R⁆ lieAlgebra b :=
   sorry
 
-end GeckConstruction
+end RootPairing.GeckConstruction
 
-end RootPairing
-
-namespace LieAlgebra
+open LieAlgebra
 
 variable {R L : Type*} [Field R] [CharZero R] [IsAlgClosed R]
   [LieRing L] [LieAlgebra R L] [FiniteDimensional R L] [LieAlgebra.IsSimple R L]
 
 open scoped Classical in
-open Module in
 /-- Weakest possible form of the classification theorem: over an algebraically closed field, every
 finite-dimensional simple Lie algebra arises from a root system. -/
 example :
     ∃ (H : LieSubalgebra R L) (_i : H.IsCartanSubalgebra)
-      (P : RootSystem H.root R (Dual R H) H) (_ : P.IsCrystallographic)
-      (b : P.Base),
+      (P : RootPairing H.root R (Dual R H) H) (_ : P.IsCrystallographic) (b : P.Base),
       Nonempty (L ≃ₗ⁅R⁆ RootPairing.GeckConstruction.lieAlgebra b) := by
   obtain ⟨x, hx⟩ := exists_isCartanSubalgebra_engel R L
-  obtain ⟨b⟩ := (IsKilling.rootSystem (LieSubalgebra.engel R x)).nonempty_base_of_isReduced
+  obtain ⟨b⟩ := (IsKilling.rootSystem (LieSubalgebra.engel R x)).nonempty_base
   exact ⟨_, hx, _, _, b, ⟨equiv_rootsystem_lieAlgebra (LieSubalgebra.engel R x) b⟩⟩
-
-end LieAlgebra
