6040bd0c616cbcb21380baaeaf5241181ef4bfd4
Oliver Nash <github@olivernash.org>
2026-03-27 10:58:44 +0000

    Tidy up a bit now that Geck construction work complete

--- LieClassification/LinearAlgebra/RootSystem/LieAlgebra.lean
+++ LieClassification/LinearAlgebra/RootSystem/LieAlgebra.lean
@@ -3,39 +3,26 @@ import Mathlib.LinearAlgebra.RootSystem.GeckConstruction.Basis
 import LieClassification.Algebra.Lie.CartanCriterion
 import LieClassification.LinearAlgebra.RootSystem.CartanMatrix
 
-open LieModule Module
+open LieModule LieAlgebra LieAlgebra.IsKilling
 
-namespace RootPairing.GeckConstruction
-
-variable {ι R M N : Type*} [Fintype ι] [DecidableEq ι] [Field R] [CharZero R] [IsAlgClosed R]
-  [AddCommGroup M] [Module R M] [AddCommGroup N] [Module R N]
-  {P : RootPairing ι R M N} [P.IsRootSystem] [P.IsReduced] [P.IsCrystallographic] [P.IsIrreducible]
-  (b : P.Base)
+variable
+  {K : Type*} [Field K] [CharZero K] [IsAlgClosed K]
+  {L : Type*} [LieRing L] [LieAlgebra K L] [FiniteDimensional K L] [IsSimple K L]
 
 open scoped Classical in
 /-- `LieAlgebra.IsKilling.rootSystem` is right inverse to `RootPairing.GeckConstruction.lieAlgebra`.
 -/
-def _root_.LieAlgebra.equiv_rootsystem_lieAlgebra {R L : Type*} [Field R] [CharZero R]
-    [LieRing L] [LieAlgebra R L] [FiniteDimensional R L] [LieAlgebra.IsSimple R L]
-    (H : LieSubalgebra R L) [H.IsCartanSubalgebra] [IsTriangularizable R H L]
+def _root_.LieAlgebra.equiv_rootsystem_lieAlgebra (H : LieSubalgebra K L) [H.IsCartanSubalgebra]
     (b : (LieAlgebra.IsKilling.rootSystem H).Base) :
-    L ≃ₗ⁅R⁆ lieAlgebra b :=
+    L ≃ₗ⁅K⁆ RootPairing.GeckConstruction.lieAlgebra b :=
   sorry
-
-end RootPairing.GeckConstruction
-
-open LieAlgebra
-
-variable {R L : Type*} [Field R] [CharZero R] [IsAlgClosed R]
-  [LieRing L] [LieAlgebra R L] [FiniteDimensional R L] [LieAlgebra.IsSimple R L]
 
 open scoped Classical in
 /-- Weakest possible form of the classification theorem: over an algebraically closed field, every
 finite-dimensional simple Lie algebra arises from a root system. -/
 example :
-    ∃ (H : LieSubalgebra R L) (_i : H.IsCartanSubalgebra)
-      (P : RootPairing H.root R (Dual R H) H) (_ : P.IsCrystallographic) (b : P.Base),
-      Nonempty (L ≃ₗ⁅R⁆ RootPairing.GeckConstruction.lieAlgebra b) := by
-  obtain ⟨x, hx⟩ := exists_isCartanSubalgebra_engel R L
-  obtain ⟨b⟩ := (IsKilling.rootSystem (LieSubalgebra.engel R x)).nonempty_base
-  exact ⟨_, hx, _, _, b, ⟨equiv_rootsystem_lieAlgebra (LieSubalgebra.engel R x) b⟩⟩
+    ∃ (H : LieSubalgebra K L) (_ : H.IsCartanSubalgebra) (b : (rootSystem H).Base),
+      Nonempty (L ≃ₗ⁅K⁆ RootPairing.GeckConstruction.lieAlgebra b) := by
+  obtain ⟨x, hx⟩ := exists_isCartanSubalgebra_engel K L
+  obtain ⟨b⟩ := (IsKilling.rootSystem (LieSubalgebra.engel K x)).nonempty_base
+  exact ⟨_, hx, b, ⟨equiv_rootsystem_lieAlgebra (LieSubalgebra.engel K x) b⟩⟩
