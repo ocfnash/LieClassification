b3ba8c29b302be3d768085921226a8a37a897464
Oliver Nash <github@olivernash.org>
2026-06-20 11:37:49 +0100

    Tidy up a bit

--- LieClassification/LinearAlgebra/RootSystem/LieAlgebra.lean
+++ LieClassification/LinearAlgebra/RootSystem/LieAlgebra.lean
@@ -1,20 +1,41 @@
-import Mathlib.Algebra.Lie.CartanExists
 import Mathlib.LinearAlgebra.RootSystem.GeckConstruction.Basis
 import Mathlib.Algebra.Lie.CartanCriterion
+import Mathlib.Algebra.Lie.CartanExists
+import Mathlib.Algebra.Lie.Weights.IsSimple
 
-open LieModule LieAlgebra LieAlgebra.IsKilling
+noncomputable section
+
+namespace LieAlgebra
+
+open IsKilling
 
 variable
   {K : Type*} [Field K] [CharZero K] [IsAlgClosed K]
   {L : Type*} [LieRing L] [LieAlgebra K L] [FiniteDimensional K L] [IsSimple K L]
 
+/-- Given two Lie algebras with distinguished Cartan subalgebras, an equivalence of the associated
+root systems determines an equivalence of the Lie algebras. -/
+def equivOfRootSystemEquiv
+    {L' : Type*} [LieRing L'] [LieAlgebra K L'] [FiniteDimensional K L'] [IsSimple K L']
+    {H : LieSubalgebra K L} [H.IsCartanSubalgebra]
+    {H' : LieSubalgebra K L'} [H'.IsCartanSubalgebra]
+    (e : (rootSystem H).Equiv (rootSystem H')) :
+    L ≃ₗ⁅K⁆ L' :=
+  sorry
+
+instance (H : LieSubalgebra K L) [H.IsCartanSubalgebra] [DecidableEq H.root]
+    (b : (rootSystem H).Base) :
+    IsSimple K (RootPairing.GeckConstruction.lieAlgebra b) := by
+  -- Trivial via `LieAlgebra.IsKilling.isSimple_iff_isIrreducible`
+  sorry
+
 open scoped Classical in
 /-- `LieAlgebra.IsKilling.rootSystem` is right inverse to `RootPairing.GeckConstruction.lieAlgebra`.
 -/
-def _root_.LieAlgebra.equiv_rootsystem_lieAlgebra (H : LieSubalgebra K L) [H.IsCartanSubalgebra]
-    (b : (LieAlgebra.IsKilling.rootSystem H).Base) :
+def equivGeckConstruction (H : LieSubalgebra K L) [H.IsCartanSubalgebra]
+    (b : (rootSystem H).Base) :
     L ≃ₗ⁅K⁆ RootPairing.GeckConstruction.lieAlgebra b :=
-  sorry
+  equivOfRootSystemEquiv <| RootPairing.GeckConstruction.equivRootSystem b
 
 open scoped Classical in
 /-- Weakest possible form of the classification theorem: over an algebraically closed field, every
@@ -23,5 +44,7 @@ example :
     ∃ (H : LieSubalgebra K L) (_ : H.IsCartanSubalgebra) (b : (rootSystem H).Base),
       Nonempty (L ≃ₗ⁅K⁆ RootPairing.GeckConstruction.lieAlgebra b) := by
   obtain ⟨x, hx⟩ := exists_isCartanSubalgebra_engel K L
-  obtain ⟨b⟩ := (IsKilling.rootSystem (LieSubalgebra.engel K x)).nonempty_base
-  exact ⟨_, hx, b, ⟨equiv_rootsystem_lieAlgebra (LieSubalgebra.engel K x) b⟩⟩
+  obtain ⟨b⟩ := (rootSystem (LieSubalgebra.engel K x)).nonempty_base
+  exact ⟨_, hx, b, ⟨equivGeckConstruction (LieSubalgebra.engel K x) b⟩⟩
+
+end LieAlgebra
